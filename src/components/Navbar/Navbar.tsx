bbd1028468ac42f16e48da513305d715c622f466
Prajwal H S <prajwalhs4india@gmail.com>
2025-02-07 13:33:39 +0530

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Logo from './Logo'
-import Menu from './Menu'
+// import Menu from './Menu'
 import "./NavBar.css"
 const NavBar = () => {
   return (
     <nav className="navbar-container">
       <Logo />
-      <Menu />
+      {/* <Menu /> */}
     </nav>
   )
 }
