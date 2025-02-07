c39c09e03a7ee5fb93e656a53ad5982169ddbdcc
Prajwal H S <prajwalhs4india@gmail.com>
2025-02-07 12:56:52 +0530

    changes1

--- src/app/doppler-scan/page.tsx
+++ src/app/doppler-scan/page.tsx
@@ -1,12 +1,14 @@
 import HeaderComponent from "@/components/HeaderComponent/HeaderComponent";
-
+import ultraSound1 from "@/Images/ultrasound/ultrasound1.png";
+import ultraSound2 from "@/Images/ultrasound/ultrasound2.png";
+import ultraSound3 from "@/Images/ultrasound/ultrasound3.png";
 export default function DopplerScan(){
     return(
         <div className="page-container">
         <HeaderComponent
-          imageSrc1={"/"}
-          imageSrc2={"/"}
-          imageSrc3={"/"}
+        imageSrc1={ultraSound1.src}
+        imageSrc2={ultraSound2.src}
+        imageSrc3={ultraSound3.src}
           alt="Doppler Banner"
           head="in Bangalore "
           highlight="Doppler Scan"
--- src/app/pathology-test/page.tsx
+++ src/app/pathology-test/page.tsx
@@ -3,14 +3,16 @@ import HomeAboutUs from "@/components/HomePage/HomeAboutUs/HomeAboutUs";
 import HomeDoctor from "@/components/HomePage/HomeDoctor/HomeDoctor";
 import HomeKey from "@/components/HomePage/HomeKey/HomeKey";
 import HomeWhy from "@/components/HomePage/HomeWhy/HomeWhy";
-
+import ultraSound1 from "@/Images/ultrasound/ultrasound1.png";
+import ultraSound2 from "@/Images/ultrasound/ultrasound2.png";
+import ultraSound3 from "@/Images/ultrasound/ultrasound3.png";
 export default function Pathology() {
   return (
     <div className="page-container">
       <HeaderComponent
-          imageSrc1={"/"}
-          imageSrc2={"/"}
-          imageSrc3={"/"}
+        imageSrc1={ultraSound1.src}
+        imageSrc2={ultraSound2.src}
+        imageSrc3={ultraSound3.src}
         alt="Pathology Banner"
         head="Test At Home"
         highlight="Blood"
