1d840463a811bf699fcd24d9a94c7fdb0a0da9af
Prajwal H S <prajwalhs4india@gmail.com>
2025-02-07 13:17:03 +0530

    removed navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import TopBar from "@/components/TopBar/Topbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "@/components/Footer/Footer";
-import NavBar from "@/components/NavBar/NavBar";
 
 export default function RootLayout({
   children,
@@ -20,7 +19,6 @@ export default function RootLayout({
       </head>
       <body>
         <TopBar />
-        <NavBar />
         {children}
         <Footer />
       </body>
