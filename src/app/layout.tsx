bdb755fafed6ef478295a87a0a34d6cfea141099
Prajwal H S <prajwalhs4india@gmail.com>
2025-02-18 13:09:51 +0530

    google color tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <meta name="theme-color" content="#0000FF" />
+
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-QBFT7KMNVD"
