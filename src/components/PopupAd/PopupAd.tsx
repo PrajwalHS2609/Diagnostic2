c41ad9bf11418d7adeb35d4125f5bf9d443ef6e8
Prajwal H S <prajwalhs4india@gmail.com>
2025-06-09 18:49:38 +0530

    ads popup update

--- src/components/PopupAd/PopupAd.tsx
+++ src/components/PopupAd/PopupAd.tsx
@@ -25,25 +25,31 @@ const PopupAd = () => {
       <div className="popupExit">
         <FaXmark className="exit" onClick={handleExit} />
       </div>
+
       <div className="popup-content">
-        <h2 className="popup-heading">🚀 BizGen Website Package</h2>
-        <h4 className="popup-subheading">
-          <span className="highlight">Launch in 24 Hrs</span> <br />
-          Only <span className="price">₹25,000</span> for a{" "}
-          <strong>fully-loaded, digital-ready</strong> website!
-        </h4>
-        <p className="popup-disclaimer">
-           This site is for sale. Please avoid inquiries about services.
-        </p>
-        <a
-          href="https://api.whatsapp.com/send?phone=916360226773"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="popup-whatsapp"
-        >
-          <FaWhatsapp className="popup-whatsIcon" />
-          <span>Grab This Deal</span>
-        </a>
+        <div className="popup-imgContainer">
+          <img src="https://img.freepik.com/free-photo/front-view-smiley-man-showing-thumbs-up_23-2148946231.jpg?uid=R153459844&ga=GA1.1.790794325.1735297516&semt=ais_items_boosted&w=740" alt="" />
+        </div>
+        <div  className="popup-contentWrapper">
+          <h2 className="popup-heading outlined-heading">🚀 BizGen Website Package</h2>
+          <h4 className="popup-subheading">
+            <span className="highlight">Launch in 24 Hrs</span> <br />
+            Only <span className="price">₹25,000</span> for a{" "}
+            <strong>fully-loaded, digital-ready</strong> website!
+          </h4>
+          <p className="popup-disclaimer">
+            This site is for sale. Please avoid inquiries about services.
+          </p>
+          <a
+            href="https://api.whatsapp.com/send?phone=916360226773"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="popup-whatsapp"
+          >
+            <FaWhatsapp className="popup-whatsIcon" />
+            <span>Grab This Deal</span>
+          </a>
+        </div>
       </div>
     </div>
   );
