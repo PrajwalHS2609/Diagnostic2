8f956b475007ffc1313ea88da06ac48eec5abfb4
Prajwal H S <prajwalhs4india@gmail.com>
2025-11-07 16:04:23 +0530

    link added

--- sanity/diagnostics2/schemaTypes/serviceContent.ts
+++ sanity/diagnostics2/schemaTypes/serviceContent.ts
@@ -1,90 +1,90 @@
-import {defineField, defineType} from 'sanity'
+  import {defineField, defineType} from 'sanity'
 
-export default defineType({
-  name: 'ServiceCategory',
-  title: 'ServiceCategory',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'body1',
-      title: 'Body1',
-      type: 'blockContent',
-    }),
-    
-    defineField({
-      name: 'body2',
-      title: 'Body2',
-      type: 'blockContent',
-    }),
-    defineField({
-      name: 'metaTitle',
-      title: 'Meta Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'metaDescription',
-      title: 'Meta Description',
-      type: 'text',
-    }),
-       defineField({
-      name: 'customTable',
-      title: 'Custom Table',
-      type: 'object',
-      fields: [
-        {
-          name: 'title',
-          title: 'Table Title',
-          type: 'string',
+  export default defineType({
+    name: 'ServiceCategory',
+    title: 'ServiceCategory',
+    type: 'document',
+    fields: [
+      defineField({
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      }),
+      defineField({
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
         },
-        {
-          name: 'headers',
-          title: 'Table Headers',
-          type: 'array',
-          of: [{type: 'string'}],
+      }),
+      defineField({
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 96,
         },
-        {
-          name: 'rows',
-          title: 'Table Rows',
-          type: 'array',
-          of: [
-            {
-              type: 'object',
-              name: 'tableRow',
-              title: 'Row',
-              fields: [
-                {
-                  name: 'cells',
-                  title: 'Cells',
-                  type: 'array',
-                  of: [{type: 'string'}],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    }),
-  ],
-})
+      }),
+      defineField({
+        name: 'body1',
+        title: 'Body1',
+        type: 'blockContent',
+      }),
+      
+      defineField({
+        name: 'body2',
+        title: 'Body2',
+        type: 'blockContent',
+      }),
+      defineField({
+        name: 'metaTitle',
+        title: 'Meta Title',
+        type: 'string',
+      }),
+      defineField({
+        name: 'metaDescription',
+        title: 'Meta Description',
+        type: 'text',
+      }),
+        defineField({
+        name: 'customTable',
+        title: 'Custom Table',
+        type: 'object',
+        fields: [
+          {
+            name: 'title',
+            title: 'Table Title',
+            type: 'string',
+          },
+          {
+            name: 'headers',
+            title: 'Table Headers',
+            type: 'array',
+            of: [{type: 'string'}],
+          },
+          {
+            name: 'rows',
+            title: 'Table Rows',
+            type: 'array',
+            of: [
+              {
+                type: 'object',
+                name: 'tableRow',
+                title: 'Row',
+                fields: [
+                  {
+                    name: 'cells',
+                    title: 'Cells',
+                    type: 'array',
+                    of: [{type: 'string'}],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      }),
+    ],
+  })
--- src/components/Services/Pathology/PathologyCard.tsx
+++ src/components/Services/Pathology/PathologyCard.tsx
@@ -9,6 +9,7 @@ const PathologyCard = () => {
       title: "Hematology",
       description:
         "Hematology is the branch of medicine that focuses on the study, diagnosis, and treatment of blood-related disorders. Our expert team ensures precise hematology services for comprehensive healthcare.",
+      link: "/hematology-blood-tests-in-bangalore",
     },
     {
       id: 2,
@@ -16,6 +17,8 @@ const PathologyCard = () => {
       title: "Biochemistry",
       description:
         "Biochemistry is the scientific study of chemical processes within and related to living organisms. Our advanced biochemistry services provide accurate insights for optimal health assessment and care.",
+
+      link: "/biochemistry-blood-tests-in-bangalore",
     },
     {
       id: 3,
@@ -23,6 +26,7 @@ const PathologyCard = () => {
       title: "Hormonal Test",
       description:
         "Hormonal tests at  Diagnostics help assess and balance crucial hormones, providing valuable insights for personalized healthcare and maintaining overall well-being.",
+      link: "/hormonal-blood-tests-in-bangalore/",
     },
     {
       id: 4,
@@ -30,6 +34,7 @@ const PathologyCard = () => {
       title: "Tumor Markers Test",
       description:
         "Tumor marker tests at  Diagnostics aid in the early detection and monitoring of cancer, enabling proactive healthcare and personalized treatment strategies for optimal outcomes.",
+      link: "/tumor-markers-blood-tests-in-bangalore",
     },
     {
       id: 5,
@@ -37,6 +42,7 @@ const PathologyCard = () => {
       title: "Micorbiology",
       description:
         "Microbiology at  Diagnostics employs advanced techniques to identify and analyze microorganisms, facilitating accurate diagnoses and tailored treatment plans for infectious diseases.",
+      link: "/microbiology-blood-tests-in-bangalore",
     },
     {
       id: 6,
@@ -44,6 +50,7 @@ const PathologyCard = () => {
       title: "Serology",
       description:
         "Serology at  Diagnostics investigates blood serum for antibodies, aiding in the diagnosis of various infections and autoimmune disorders with precision and comprehensive understanding.",
+      link: "/serology -blood-tests-in-bangalore",
     },
     {
       id: 7,
@@ -51,6 +58,7 @@ const PathologyCard = () => {
       title: "Clinical Pathology",
       description:
         "Clinical Pathology at  Diagnostics focuses on analyzing bodily fluids and tissues, providing crucial insights for accurate disease diagnosis and optimal patient care.",
+      link: "",
     },
     {
       id: 8,
@@ -58,29 +66,28 @@ const PathologyCard = () => {
       title: "Book Blood Tests at Home",
       description:
         "Experience convenience with  Diagnostics – Blood Tests at Home. Book now for accurate and hassle-free healthcare services in the comfort of your space.",
+      link: "",
     },
   ];
   return (
     <div className="subService-container">
-     {card.map((x)=>(
-         <div className="subService-content" key={x.id}>
-         <div className="subService-item">
-           <img
-             src={x.img}
-             alt={x.title}
-           />
-         </div>
-         <div className="subService-item">
-           <h4>{x.title}</h4>
-           <p>
-            {x.description}
-           </p>
-           <Link href="/">
-             <button>Read More</button>
-           </Link>
-         </div>
-       </div>
-     ))}
+      {card.map((x) => (
+        <div className="subService-content" key={x.id}>
+          <div className="subService-item">
+            <img src={x.img} alt={x.title} />
+          </div>
+          <div className="subService-item">
+            <h4>
+              {" "}
+              <Link href={x.link}>{x.title}</Link>{" "}
+            </h4>
+            <p>{x.description}</p>
+            <Link href={x.link}>
+              <button>Read More</button>
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
